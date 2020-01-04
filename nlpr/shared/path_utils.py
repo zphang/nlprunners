8bf816ef4269af055d42979435ccec16466b0cac
Jason Phang <email@jasonphang.com>
2020-01-04 18:35:40 -0500

    more testing

--- nlpr/shared/path_utils.py
+++ nlpr/shared/path_utils.py
@@ -3,3 +3,11 @@ import os
 
 def format_path_from_env(path_str):
     return path_str.replace("$", "").format(os.environ)
+
+
+def get_nlpr_base_path():
+    return os.path.abspath(os.path.join(os.path.abspath(__file__), "..", "..", ".."))
+
+
+def get_nlpr_path(path_stubs):
+    return os.path.abspath(os.path.join(get_nlpr_base_path(), *path_stubs))
--- nlpr/testing_tools/run_example_processing.py
+++ nlpr/testing_tools/run_example_processing.py
@@ -1,32 +1,39 @@
-import os
-
 import zconf
 
 import nlpr.testing_tools.example_processing as example_processing
+import nlpr.shared.path_utils as path_utils
 
 
 @zconf.run_config
 class RunConfiguration(zconf.RunConfig):
     model_config_base_path = zconf.attr(type=str, required=True)
-    task_config_base_path = zconf.attr(type=str, required=True)
+    task_config_base_path = zconf.attr(type=str, default=None)
     output_base_path = zconf.attr(type=str, required=True)
     reference_base_path = zconf.attr(type=str, default=None)
 
 
-def get_default_examples_path():
-    return os.path.abspath(os.path.join(
-        os.path.abspath(__file__), "..", "..", "..", "test_data", "example_processing",
-    ))
+def get_default_reference_base():
+    return path_utils.get_nlpr_path(["test_data", "example_processing"])
+
+
+def get_default_task_config_base_path():
+    return path_utils.get_nlpr_path(["test_data", "example_processing", "inputs", "task_configs"])
 
 
 def run_checks(args: RunConfiguration):
     if args.reference_base_path is None:
-        reference_base_path = get_default_examples_path()
+        reference_base_path = get_default_reference_base()
     else:
         reference_base_path = args.reference_base_path
+
+    if args.task_config_base_path is None:
+        task_config_base_path = get_default_task_config_base_path()
+    else:
+        task_config_base_path = args.task_config_base_path
+
     example_processing.write_out(
         model_config_base_path=args.model_config_base_path,
-        task_config_base_path=args.task_config_base_path,
+        task_config_base_path=task_config_base_path,
         output_base_path=args.output_base_path,
     )
     example_processing.run_checks(
