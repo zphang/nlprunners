e2147e83e0883d173c558dea42732b2a8447c8b8
Jason Phang <email@jasonphang.com>
2020-02-13 11:24:08 -0500

    smart truncate

--- nlpr/proj/simple/tokenize_and_cache.py
+++ nlpr/proj/simple/tokenize_and_cache.py
@@ -1,6 +1,8 @@
 import os
+import numpy as np
 
 import zconf
+import pyutils.io as io
 
 import nlpr.tasks as tasks
 import nlpr.shared.runner as shared_runner
@@ -8,6 +10,7 @@ import nlpr.shared.model_resolution as shared_model_resolution
 import nlpr.shared.model_setup as shared_model_setup
 import nlpr.shared.caching as shared_caching
 import nlpr.tasks.evaluate as evaluate
+import nlpr.shared.torch_utils as torch_utils
 
 
 @zconf.run_config
@@ -23,9 +26,10 @@ class RunConfiguration(zconf.RunConfig):
     max_seq_length = zconf.attr(default=128, type=int)
     chunk_size = zconf.attr(default=10000, type=int)
     force_overwrite = zconf.attr(action="store_true")
+    smart_truncate = zconf.attr(action="store_true")
 
 
-def chunk_and_save(phase, examples, feat_spec, tokenizer, args):
+def chunk_and_save(phase, examples, feat_spec, tokenizer, args: RunConfiguration):
     dataset = shared_runner.convert_examples_to_dataset(
         examples=examples,
         feat_spec=feat_spec,
@@ -33,12 +37,59 @@ def chunk_and_save(phase, examples, feat_spec, tokenizer, args):
         phase=phase,
         verbose=True,
     )
+    if args.smart_truncate:
+        dataset, length = experimental_smart_truncate(
+            dataset=dataset,
+            max_seq_length=args.max_seq_length,
+        )
+        os.makedirs(os.path.join(args.output_dir, phase), exist_ok=True)
+        io.write_json(
+            data={"truncated_to": int(length)},
+            path=os.path.join(args.output_dir, phase, "smart_truncate.json"),
+        )
     shared_caching.chunk_and_save(
         data=dataset.data,
         chunk_size=args.chunk_size,
         data_args=args.to_dict(),
         output_dir=os.path.join(args.output_dir, phase),
     )
+
+
+def experimental_smart_truncate(dataset: torch_utils.ListDataset,
+                                max_seq_length: int):
+    if "input_mask" not in dataset.data[0]["data_row"].get_fields():
+        raise RuntimeError("Smart truncate not supported")
+    max_valid_length_ls = []
+    range_idx = np.arange(max_seq_length)
+    for datum in dataset.data:
+        indexer = datum["data_row"].input_mask.reshape(-1, max_seq_length).max(-2)
+        max_valid_length_ls.append(range_idx[indexer.astype(bool)].max() + 1)
+    max_valid_length = max(max_valid_length_ls)
+
+    if max_valid_length == max_seq_length:
+        return dataset, max_seq_length
+
+    new_datum_ls = []
+    for datum in dataset.data:
+        row_dict = datum["data_row"].asdict()
+        new_row_dict = row_dict.copy()
+        for k, v in row_dict.items():
+            if not isinstance(v, np.ndarray):
+                continue
+            if max_seq_length not in v.shape:
+                continue
+            if not v.shape.count(max_seq_length) == 1:
+                raise RuntimeError("confusing dimensions")
+            slice_ls = []
+            for n in v.shape:
+                if n == max_seq_length:
+                    slice_ls.append(slice(None, max_valid_length))
+                else:
+                    slice_ls.append(slice(None))
+            new_row_dict[k] = v[tuple(slice_ls)]
+        new_datum_ls.append(datum["data_row"].__class__(**new_row_dict))
+    new_dataset = torch_utils.ListDataset(new_datum_ls)
+    return new_dataset, max_valid_length
 
 
 def main(args: RunConfiguration):
